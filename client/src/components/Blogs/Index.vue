fc811736b7abed0c8e0a0d168724d3f9a9f68376
Navakun <kunkid28147@gmail.com>
2024-10-10 04:30:48 +0700

    update 4.30 AM

--- client/src/components/Blogs/Index.vue
+++ client/src/components/Blogs/Index.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>
+    <!-- <div>
         <h2>Get all blogs</h2>
         <p><button v-on:click="logout">Logout</button></p>
-        <h4>จำนวน blog {{blogs.length}}</h4>
+        <h4>จำนวน blog {{ blogs.length }}</h4>
+        <div v-if="blogs.length === 0" class="empty-blog">
+            *** ไม่มีข้อมูล ***
+        </div>
         <p><button v-on:click="navigateTo('/blog/create')">สร้าง blog</button></p>
         <div v-for="blog in blogs" v-bind:key="blog.id">
             <p>id: {{ blog.id }}</p>
@@ -11,52 +14,142 @@
             <p>category: {{ blog.category }}</p>
             <p>status: {{ blog.status }}</p>
             <p>
-            <button v-on:click="navigateTo('/blog/'+ blog.id)">ดู blog</button>
-            <button v-on:click="navigateTo('/blog/edit/'+ blog.id)">แก้ไข blog</button>
-            <button v-on:click="deleteBlog(blog)">ลบข้อมูล</button>
+                <button v-on:click="navigateTo('/blog/' + blog.id)">ดู blog</button>
+                <button v-on:click="navigateTo('/blog/edit/' + blog.id)">แก้ไข blog</button>
+                <button v-on:click="deleteBlog(blog)">ลบข้อมูล</button>
             </p>
             <hr>
+        </div>
+    </div> -->
+    <div>
+        <div class="blog-header">
+            <h2>สวนจดการบลอก</h2>
+            <div>
+                <button v-on:click="navigateTo('/blog/create')">create
+                    blog</button>
+                <strong> จํานวน blog: </strong> {{ blogs.length }}
+            </div>
+            <br>
+        </div>
+        <div v-if="blogs.length === 0" class="empty-blog">
+            *** ไมมขอมล ***
+        </div>
+        <div v-for="blog in blogs" v-bind:key="blog.id" class="blog-list">
+            <!-- <p>id: {{ blog.id }}</p> -->
+            <div class="blog-pic">
+                <transition name="fade">
+                    <div class="thumbnail-pic" v-if="blog.thumbnail != 'null'">
+                        <img :src="BASE_URL + blog.thumbnail" alt="thumbnail">
+                    </div>
+                </transition>
+            </div>
+            <h3>{{ blog.title }}</h3>
+            <div v-html="blog.content.slice(0, 200)"></div>
+            <div class="blog-info">
+                <p><strong>Category:</strong> {{ blog.category }}</p>
+                <p><strong>Create:</strong> {{ blog.createdAt }}</p>
+                <!-- <p>status: {{ blog.status }}</p> -->
+
+                <p>
+                    <button v-on:click="navigateTo('/blog/' + blog.id)">ดู
+                        blog</button>
+                    <button v-on:click="navigateTo('/blog/edit/' + blog.id)">แกไข
+                        blog</button>
+                    <button v-on:click="deleteBlog(blog)">ลบข้อมูล</button>
+                </p>
+            </div>
+            <div class="clearfix"></div>
         </div>
     </div>
 </template>
 <script>
-    import BlogsService from '@/services/BlogsService'
-    export default {
-        data () {
-            return {
-                blogs: []
-            }
+import BlogsService from '@/services/BlogsService'
+export default {
+    data() {
+        return {
+            blogs: [],
+            BASE_URL: "http://localhost:8081/assets/uploads/",
+        }
+    },
+    async created() {
+        this.blogs = (await BlogsService.index()).data
+    },
+    methods: {
+        logout() {
+            this.$store.dispatch('setToken', null)
+            this.$store.dispatch('setBlog', null)
+            this.$router.push({
+                name: 'login'
+            })
         },
-        async created () {
-            this.blogs = (await BlogsService.index()).data
+        navigateTo(route) {
+            this.$router.push(route)
         },
-        methods: {
-            logout () {
-                this.$store.dispatch('setToken', null)
-                this.$store.dispatch('setBlog', null)
-                this.$router.push({
-                    name: 'login'
-                })
-            },
-            navigateTo (route) {
-                this.$router.push(route)
-            },
-            async deleteBlog (blog) {
-                let result = confirm("Want to delete?")
-                if (result) {
-                    try {
-                        await BlogsService.delete(blog)
-                        this.refreshData()
-                    } catch (err) {
-                        console.log(err)
-                    }
+        async deleteBlog(blog) {
+            let result = confirm("Want to delete?")
+            if (result) {
+                try {
+                    await BlogsService.delete(blog)
+                    this.refreshData()
+                } catch (err) {
+                    console.log(err)
                 }
-            },
-            async refreshData() {
-                this.blogs = (await BlogsService.index()).data
             }
+        },
+        async refreshData() {
+            this.blogs = (await BlogsService.index()).data
         }
     }
+}
 </script>
 <style scoped>
+.empty-blog {
+    width: 100%;
+    text-align: center;
+    padding: 10px;
+    background: darksalmon;
+    color: white;
+}
+
+.empty-blog {
+    width: 100%;
+    text-align: center;
+    padding: 10px;
+    background: darksalmon;
+    color: white;
+}
+
+/* thumbnail */
+.thumbnail-pic img {
+    width: 200px;
+    padding: 5px 10px 0px 0px;
+}
+
+.blog-info {
+    float: left;
+}
+
+.blog-pic {
+    float: left;
+}
+
+.clearfix {
+    clear: both;
+}
+
+.blog-list {
+    border: solid 1px #dfdfdf;
+    margin-bottom: 10px;
+    max-width: 900px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 5px;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .1);
+}
+
+.blog-header {
+    max-width: 900px;
+    margin-left: auto;
+    margin-right: auto;
+}
 </style>
